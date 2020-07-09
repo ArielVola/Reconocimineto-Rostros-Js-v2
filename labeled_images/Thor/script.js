ec2ece7e4534e3414f9a8a607cd402cca5b920f1
ArielVola <ariel18vola@gmail.com>
2020-07-08 23:30:45 -0300

    das

--- labeled_images/Thor/script.js
+++ labeled_images/Thor/script.js
@@ -37,7 +37,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-    const labels = ['Capitan America', 'Chino', 'Thor', 'Aylin', 'Ariel Vola']
+    const labels = ['Capitan America', 'Chino', 'Thor', 'Aylin', 'Ariel Vola', 'Joao Paura']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
