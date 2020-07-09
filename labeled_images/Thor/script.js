5b9963b19d2f3cf24841c7f5d3db5f415973f03e
ArielVola <ariel18vola@gmail.com>
2020-07-08 22:35:17 -0300

    gran avance

--- labeled_images/Thor/script.js
+++ labeled_images/Thor/script.js
@@ -7,36 +7,47 @@ Promise.all ([
 
 ]).then(start)
 
-function start() {
+async function start() {
     const container = document.createElement('div')
-    container.style.position = 'relative'
-    document.body.append(container)
-
-     document.body.append('Cargado')
-     imageUpload.addEventListener('change', async () => {
-         const image = await faceapi.bufferToImage(imageUpload.files[0])
-         container.append(image)
-         const canvas = faceapi.createCanvasFromMedia(image)
-        container.append(canvas)
-        const displaySize = { width: image.width, height: image.height }
-        faceapi.matchDimensions(canvas, displaySize)
-         const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-         const resizedDetections = faceapi.resizeResults(detections, displaySize)
-         resizedDetections.forEach(detection => {
-             const box = detection.detection.box
-             const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' })
-             drawBox.draw(canvas)
-         })   
-     })
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Cargado')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+    })
+  })
 }
 
 function loadLabeledImages() {
-    const labels = ['Thor', 'Capitan America']
-    return Promise.all([
-     labels.map(async label => {
-         for (let i = 1; i <= 2; i++) {
-            const img = await faceapi.fetchImage()//min 15 35
-         }
-     })   
-    ])
+    const labels = ['Capitan America', 'Thor']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ArielVola/Reconocimineto-Rostros-Js-v2/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
 }
